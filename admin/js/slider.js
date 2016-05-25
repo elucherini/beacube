49d78cfd370c2cc4c72fa059ad3f5c43923a4d0f
Elena Lucherini <eleluche@gmail.com>
2016-05-25 16:28:57 +0200

    Range on web interface corrected

--- admin/js/slider.js
+++ admin/js/slider.js
@@ -2,16 +2,32 @@ var rangeSlider = function(){
   var slider = $('.range-slider'),
       range = $('.range-slider__range'),
       value = $('.range-slider__value');
+  var string = '';
     
   slider.each(function(){
 
     value.each(function(){
       var value = $(this).prev().attr('value');
-	  $(this).html(value);
+  if (value <= 2)
+	  string = 'immediate<br>2~5 m';
+  else if (value > 2 && value <= 5)
+	  string = 'near<br>5~8 m';
+  else if (value > 5)
+	  string = 'far<br>>8 m';
+	  $(this).html(string);
     });
 
     range.on('input', function(){
-      $(this).next(value).html(this.value);
+      var currVal = value.prev().attr('value');
+	  if (currVal <= 2)
+		  string = 'immediate<br>2~5 m';
+	  else if (currVal > 2 && currVal <= 5)
+		  string = 'near<br>5~8 m';
+	  else if (currVal > 5)
+		  string = 'far<br>>8 m';
+		  $(this).html(string);
+	
+      $(this).next(value).html(string);
     });
   });
 };
