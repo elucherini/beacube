5acffd4e3e099f7f10d81d4619c4cdcdac880349
Elena Lucherini <eleluche@gmail.com>
2016-05-28 10:02:52 +0200

    Fixes on slider

--- admin/js/slider.js
+++ admin/js/slider.js
@@ -9,22 +9,22 @@ var rangeSlider = function(){
     value.each(function(){
       var value = $(this).prev().attr('value');
   if (value <= 2)
-	  string = 'immediate<br>2~5 m';
+	  string = 'immediate<br>< 2 m';
   else if (value > 2 && value <= 5)
-	  string = 'near<br>5~8 m';
-  else if (value > 5)
-	  string = 'far<br>>8 m';
+	  string = 'near<br>2~5 m';
+  else if (value > 5 && value <= 8)
+	  string = 'far<br>5~8 m';
 	  $(this).html(string);
     });
 
     range.on('input', function(){
       var currVal = value.prev().attr('value');
 	  if (currVal <= 2)
-		  string = 'immediate<br>2~5 m';
+		  string = 'immediate<br>< 2 m';
 	  else if (currVal > 2 && currVal <= 5)
-		  string = 'near<br>5~8 m';
+		  string = 'near<br>2~5 m';
 	  else if (currVal > 5)
-		  string = 'far<br>>8 m';
+		  string = 'far<br>5~8 m';
 		  $(this).html(string);
 	
       $(this).next(value).html(string);
